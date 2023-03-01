7b6f35ed4096a944ab47c5095294cbaed62e4e19
Eray Şahin <nurieraysahin@gmail.com>
2023-03-02 02:04:07 +0300

    Updates

---
+++ precache-manifest.ba5fca7fb0c6871ad153fa1f8156526d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "142be34169a83c10fae5",
+    "url": "/qr-scanner-pwa/static/css/main.55515d0b.chunk.css"
+  },
+  {
+    "revision": "142be34169a83c10fae5",
+    "url": "/qr-scanner-pwa/static/js/main.142be341.chunk.js"
+  },
+  {
+    "revision": "4e8082a11f9b6a9c094c",
+    "url": "/qr-scanner-pwa/static/js/1.4e8082a1.chunk.js"
+  },
+  {
+    "revision": "21365e56adcc7275c583",
+    "url": "/qr-scanner-pwa/static/js/runtime~main.21365e56.js"
+  },
+  {
+    "revision": "3b6bcab94406905b297aa4fd4efa3c95",
+    "url": "/qr-scanner-pwa/index.html"
+  }
+];
